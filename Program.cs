9c346569349cbd554a1999b59d8e00db429f19fc
Reno Neto <renoneto123@gmail.com>
2024-08-28 15:06:43 -0300

    programa corrigido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("(99)999999999","Modelo Nokia","999999999999",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone("(77)977777777","Modelo iPhone","777777777777",256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
